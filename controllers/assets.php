caa5002f5599d9fe3483fa112f17903b21a9819d
LKristo <kristo.lall@khk.ee>
2017-02-14 11:27:57 +0200

    Esialgne commit

---
+++ controllers/assets.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kpc
+ * Date: 14/02/2017
+ * Time: 11:09
+ */
+
+namespace Halo;
+
+
+class assets extends Controller
+{
+
+
+    function index(){
+
+    }
+
+}
